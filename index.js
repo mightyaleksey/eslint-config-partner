2540ec0cca602301f481d02dfcf588e088c7a07b
Alexey Litvinov <sullenor@yandex-team.ru>
2017-06-09 13:37:29 +0300

    partner object-curly free

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ module.exports = {
     'brace-style': [2, '1tbs', {allowSingleLine: true}],
     curly: [2, 'all'],
     indent: [2, 4],
-    'object-curly-newline': [2, {minProperties: 5, multiline: true}],
   },
 };

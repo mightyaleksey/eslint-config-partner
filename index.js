cc5d66f87bfd874d4c53fadc870f40c1213572ab
Alexey Litvinov <sullenor@yandex-team.ru>
2017-06-09 13:35:25 +0300

    object-curly-newline for *-partner

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 module.exports = {
   extends: '@sullenor/eslint-config',
   rules: {
+    'brace-style': [2, '1tbs', {allowSingleLine: true}],
     curly: [2, 'all'],
     indent: [2, 4],
+    'object-curly-newline': [2, {minProperties: 5, multiline: true}],
   },
 };
